9a539b15170022ad413d3f6e9c723bb41e94dafa
Timformation <Timformation@users.noreply.github.com>
2017-06-20 18:14:11 -0700

    getCityState
    Utilizes ZIP code.

---
+++ getCityState/getCityState/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace getCityState
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetCityState = new System.Windows.Forms.Button();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetCityState
+            // 
+            this.btnGetCityState.Location = new System.Drawing.Point(323, 265);
+            this.btnGetCityState.Name = "btnGetCityState";
+            this.btnGetCityState.Size = new System.Drawing.Size(164, 52);
+            this.btnGetCityState.TabIndex = 0;
+            this.btnGetCityState.Text = "Get City/State";
+            this.btnGetCityState.UseVisualStyleBackColor = true;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(323, 176);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(100, 31);
+            this.txtZip.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 448);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.btnGetCityState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetCityState;
+        private System.Windows.Forms.TextBox txtZip;
+    }
+}
+
---
+++ getCityState/getCityState/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace getCityState
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            {
+                getCityState(int.Parse(txtZip.Text));
+            }
+            private string getCityState(int Zip);
+
+            String cnStr = "Data Source=134.39.173.35;Initial Catalog=tsirchuk_w17;User ID=tsirchuk_w17;password=Checkmate!";
+
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = cnStr;
+
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("Select city,state,zip from tblZipcodes where zip='" + Zip + "'", cn);
+            SqlDataReader rdrZip;
+            try
+            {
+                rdrZip = cmd.ExecuteReader();
+                if (!rdrZip.HasRows) 
+                
+               
+            {
+
+
+                MessageBox.Show("Error.");
+
+            }
+        
+
+            
+            string city = rdrZip["city"].ToString();
+            string state = rdrZip["state"].ToString();
+
+            string cityState = city + ", " + state;
+            return cityState;
+        
+        }
+    }
+}
+    
